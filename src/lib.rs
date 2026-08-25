6bceb8bf15f778a2345fefd1736554c860e3d923
Karl Michael Figuerrez <69154024+kmfiguerrez@users.noreply.github.com>
2026-08-25 06:20:07 +0000

    Add ipv4 module.
    Added IPv4 struct and parse_ipv4_string associate
    function.
    
    Added an entry in gitignore file.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,32 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+mod ipv4 {
+    pub struct Ipv4;
+
+    impl Ipv4 {
+        pub fn parse_string_ipv4(ipv4_address: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>>  {
+            let octets = ipv4_address
+                .split('.')
+                .map(|octet| octet.parse::<u8>())
+                .collect::<Result<Vec<_>, _>>()?;
+
+            if octets.len() != 4 {
+                return Err("IPv4 address must contain exactly 4 octets".into());
+            }
+
+            Ok(octets)
+        }
+    }
 }
+
+pub use ipv4::Ipv4;
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn parse_string_ipv4_valid_address() {
+        let result = Ipv4::parse_string_ipv4("192.168.1.10");
+
+        assert_eq!(result.unwrap(), vec![192, 168, 1, 10]);
     }
 }
