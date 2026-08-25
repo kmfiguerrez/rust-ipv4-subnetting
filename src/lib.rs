36a0d51222e8d0f1738f09f748dd3bcd92b506bd
Karl Michael Figuerrez <69154024+kmfiguerrez@users.noreply.github.com>
2026-08-25 08:45:47 +0000

    add is_ipv4 associate function.
    Modified the test names.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,11 @@ mod ipv4 {
     pub struct Ipv4;
 
     impl Ipv4 {
+        /// Returns true if the argument is valid, otherwise false.
+        pub fn is_ipv4(ipv4_address: &str) -> bool {
+            Ipv4::parse_string_ipv4(ipv4_address).is_ok()
+        }
+
         pub fn parse_string_ipv4(ipv4_address: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>>  {
             let octets = ipv4_address
                 .split('.')
@@ -24,9 +29,31 @@ mod tests {
     use super::*;
 
     #[test]
-    fn parse_string_ipv4_valid_address() {
-        let result = Ipv4::parse_string_ipv4("192.168.1.10");
-
-        assert_eq!(result.unwrap(), vec![192, 168, 1, 10]);
+    fn is_ipv4_rejects_empty_string() {
+        assert!(!Ipv4::is_ipv4(""));
     }
+
+    #[test]
+    fn is_ipv4_rejects_octet_greater_than_255() {
+        let result = Ipv4::is_ipv4("192.168.1.256");
+
+        assert!(!result);
+    }
+
+    #[test]
+    fn is_ipv4_rejects_non_numeric_octet() {
+        let result = Ipv4::is_ipv4("192.168.hello.10");
+
+        assert!(!result);
+    }
+
+    #[test]
+    fn is_ipv4_rejects_too_few_octets() {
+        assert!(!Ipv4::is_ipv4("192.168.1"));
+    }
+
+    #[test]
+    fn is_ipv4_rejects_too_many_octets() {
+        assert!(!Ipv4::is_ipv4("192.168.1.10.20"));
+    }    
 }
